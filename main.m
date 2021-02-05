e216d643af598ad1ea5b89ed848bf8f9c725e4b8
Filipi Silva <filipinascimento@gmail.com>
2021-02-05 10:00:25 -0500

    Added example parameter (threshold).

--- main.m
+++ main.m
@@ -2,10 +2,19 @@ function [] = main(configJsonFilename)
   % load our own config.json
   config = loadjson(configJsonFilename);
   
+  % Getting input network
   networkPath = '';
   if isfield(config,'network')
       networkPath = config.network;
   end
+
+
+  % Getting threshold parameter
+  threshold = 0.0;
+  if isfield(config,'threshold')
+    threshold = config.threshold;
+  end
+
   mkdir("output")
 
   graph = JGFGraph(networkPath)
@@ -17,6 +26,9 @@ function [] = main(configJsonFilename)
   disp(graph.weightNames)
   
   w = graph.weightMatrix;
+
+  % Using threshold
+  w(w<threshold) = 0;
 
   % Calculating node strength
   strengths = sum(w,1);
